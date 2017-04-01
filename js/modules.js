72a2a6a04de5be51411b161defbe54d7b5ea35a7
Matthew Clayson <cla16066@byui.edu>
2017-04-01 10:37:26 -0600

    Project update

---
+++ js/modules.js
@@ -0,0 +1,6 @@
+// Include modules into the html file
+// Pathes may need altered depending on site structure
+$(function () {
+  $("#page-header").load("../modules/page-header.html");
+  $("#page-footer").load("../modules/page-footer.html");
+});
