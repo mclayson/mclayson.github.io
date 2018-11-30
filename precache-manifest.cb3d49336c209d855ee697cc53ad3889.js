7eb87ae1338be44dab6bdc5bdf250b6e40813757
Matt Clayson <matthew.clayson@gmail.com>
2018-11-30 08:52:30 -0800

    Added Production Build for testing

---
+++ precache-manifest.cb3d49336c209d855ee697cc53ad3889.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "694152e31cb4a00f7634f43b7044d889",
+    "url": "./static/media/logo.694152e3.png"
+  },
+  {
+    "revision": "cfc4ecc9e79cfe0f86b8972157182bdd",
+    "url": "./static/media/loading-animation.cfc4ecc9.gif"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "523a4c123a5dc11a372a",
+    "url": "./static/js/main.523a4c12.chunk.js"
+  },
+  {
+    "revision": "812531333681220fcc05",
+    "url": "./static/js/1.81253133.chunk.js"
+  },
+  {
+    "revision": "523a4c123a5dc11a372a",
+    "url": "./static/css/main.c21baf21.chunk.css"
+  },
+  {
+    "revision": "9c5bad29b97d9d3a6c44331c3d5f30fd",
+    "url": "./index.html"
+  }
+];
